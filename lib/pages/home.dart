37130302a22997378de644ea9dd1a267acc10059
seven <lc8681@gmail.com>
2019-07-26 18:43:41 +0800

    about page Done

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -139,6 +139,87 @@ class MyHomeState extends State<HomePage>
             SizedBox(width: 20),
           ],
         ),
+        drawer: Drawer(
+          //侧边栏按钮Drawer
+          child: ListView(
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                //Material内置控件
+                accountName: Text('CYC'), //用户名
+                accountEmail: Text('example@126.com'), //用户邮箱
+                currentAccountPicture: GestureDetector(
+                  //用户头像
+                  onTap: () => print('current user'),
+                  child: CircleAvatar(
+                    //圆形图标控件
+                    backgroundImage: NetworkImage(
+                        'https://upload.jianshu.io/users/upload_avatars/7700793/dbcf94ba-9e63-4fcf-aa77-361644dd5a87?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240'), //图片调取自网络
+                  ),
+                ),
+                otherAccountsPictures: <Widget>[
+                  //粉丝头像
+                  GestureDetector(
+                    //手势探测器，可以识别各种手势，这里只用到了onTap
+                    onTap: () => print('other user'), //暂且先打印一下信息吧，以后再添加跳转页面的逻辑
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://upload.jianshu.io/users/upload_avatars/10878817/240ab127-e41b-496b-80d6-fc6c0c99f291?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240'),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => print('other user'),
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://upload.jianshu.io/users/upload_avatars/8346438/e3e45f12-b3c2-45a1-95ac-a608fa3b8960?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240'),
+                    ),
+                  ),
+                ],
+                decoration: BoxDecoration(
+                  //用一个BoxDecoration装饰器提供背景图片
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    // image: new NetworkImage('https://raw.githubusercontent.com/flutter/website/master/_includes/code/layout/lakes/images/lake.jpg')
+                    //可以试试图片调取自本地。调用本地资源，需要到pubspec.yaml中配置文件路径
+                    image: ExactAssetImage('images/lake.jpg'),
+                  ),
+                ),
+              ),
+              ListTile(
+                  //第一个功能项
+                  title: Text('First Page'),
+                  trailing: Icon(Icons.arrow_upward),
+                  onTap: () {
+                    Navigator.of(context).pop();
+//                    Navigator.of(context).push(new MaterialPageRoute(
+//                        builder: (BuildContext context) => new SidebarPage()));
+                  }),
+              ListTile(
+                  //第二个功能项
+                  title: Text('Second Page'),
+                  trailing: Icon(Icons.arrow_right),
+                  onTap: () {
+                    Navigator.of(context).pop();
+//                    Navigator.of(context).push(new MaterialPageRoute(
+//                        builder: (BuildContext context) => new SidebarPage()));
+                  }),
+              ListTile(
+                  //第二个功能项
+                  title: Text('Second Page'),
+                  trailing: Icon(Icons.arrow_right),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                    Navigator.of(context).pushNamed('/a');
+                  }),
+              Divider(), //分割线控件
+              ListTile(
+                //退出按钮
+                title: Text('Close'),
+                trailing: Icon(Icons.cancel),
+                onTap: () => Navigator.of(context).pop(), //点击后收起侧边栏
+              ),
+            ],
+          ),
+        ),
 
         // Set the TabBar view as the body of the Scaffold
         body: new TabBarView(
@@ -273,23 +354,23 @@ class MyHomeState extends State<HomePage>
     return showDialog(
           context: context,
           builder: (context) => AlertDialog(
-                title: Text('确定退出吗?'),
-                content: Text('退出后将不能收到最新的拼车信息'),
-                actions: <Widget>[
-                  FlatButton(
-                    onPressed: () {
-                      Navigator.of(context).pop(true);
-                    },
-                    child: new Text('是'),
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      Navigator.of(context).pop(false);
-                    },
-                    child: new Text('否'),
-                  ),
-                ],
+            title: Text('确定退出吗?'),
+            content: Text('退出后将不能收到最新的拼车信息'),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: new Text('是'),
               ),
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: new Text('否'),
+              ),
+            ],
+          ),
         ) ??
         false;
   }
